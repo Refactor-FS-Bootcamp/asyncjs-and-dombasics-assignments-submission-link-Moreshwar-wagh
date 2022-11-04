ef49747f4e603e1a5a76cdef48fe553e15d8a21d
Moreshwar Wagh <moreshwarswagh@gmail.com>
2022-11-03 23:44:15 -0700

    Assign 1
    Converter App

---
+++ conterterkgtopound/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  document.getElementById("kgsInput").addEventListener("input", e => {
+    let kgs = e.target.value;
+    document.getElementById("lbsOutput").innerText = kgs * 2.205
+  })
+
+  document.getElementById("feetInput").addEventListener("input", e => {
+    let feet = e.target.value;
+    document.getElementById("cmsOutput").innerText = feet * 30.48
+  })
+}
+
+export default App;
